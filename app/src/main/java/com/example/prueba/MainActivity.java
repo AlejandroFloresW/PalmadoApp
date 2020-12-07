d811f83fddfc7b38068de012b7c0f7edb83f6ac8
AlejandroFlores <yaro.flores8816@est.uca.edu.ni>
2020-12-06 22:58:58 -0600

    quinto commit (Recycler View falta mostrar la lista y ya no sabemos como, AYUDAAAAAA :'c)

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import  com.example.prueba.data.*;
+import com.example.prueba.models.*;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<actividadesModel> mModelList;
+    private actividadesAssetSource assets;
+
+    private static final String TAG = MainActivity.class.getName();
 
     private BottomNavigationView bottomNavigationView;
     @Override
@@ -15,4 +27,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    private  void setup(){
+        assets = new actividadesAssetSource(getBaseContext());
+        mModelList = new ArrayList<>();
+    }
+
+    @Override
+
+    protected void onResume() {
+        super.onResume();
+        loadData();
+    }
+
+    private void loadData() {
+        if(!mModelList.isEmpty()) {
+            Log.d(TAG, "Ya existen valores en la lista");
+            return;
+        }
+        mModelList = assets.getAll();
+        loadRecycleFragment(new ArrayList<actividadesModel>(mModelList));
+    }
+
+    private void loadRecycleFragment(ArrayList<actividadesModel> points) {
+        FragmentTransaction frgTran = getSupportFragmentManager().beginTransaction();
+        frgTran.replace(R.id.fundations_ph, fundationFragment.newInstance(points));
+        frgTran.commit();
+    }
+
 }
